efe46492a45dbe5317466537977761418c38a1cd
Ghedyvan <gvbs@ic.ufal.br>
2025-05-18 04:29:25 -0300

    feat: Adiciona configuração de watch para o bot com arquivos ignorados

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,31 @@ module.exports = {
     {
       name: "bot",
       script: "bot.js",
-      watch: false,
+      watch: {
+        ignored: [
+          "node_modules",
+          "*.json",
+          "*.log",
+          "logs/*",
+          "assets/*",
+          "*.txt",
+        ],
+      },
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "production",
       },
-      restart_delay: 10000,  // 10 segundos de espera antes de reiniciar em caso de falha
-      max_restarts: 10,      // Máximo de 10 reinicializações em caso de falhas contínuas
-      autorestart: true
+      restart_delay: 10000, // 10 segundos de espera antes de reiniciar em caso de falha
+      max_restarts: 10, // Máximo de 10 reinicializações em caso de falhas contínuas
+      autorestart: true,
     },
     {
       name: "watchdog",
       script: "watchdog.js",
       watch: false,
-      restart_delay: 30000,  // 30 segundos de espera antes de reiniciar o watchdog
+      restart_delay: 30000, // 30 segundos de espera antes de reiniciar o watchdog
       max_restarts: 5,
-      autorestart: true
-    }
-  ]
+      autorestart: true,
+    },
+  ],
 };
